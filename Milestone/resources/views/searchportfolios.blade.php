ea36e0e4430758b6ae03bcccf03743dfc3cddcb1
TBYT <TBYTBiz@googlegroups.com>
2021-02-27 22:54:36 -0700

    Update searchportfolios.blade.php
    Made search results page look better.

--- Milestone/resources/views/searchportfolios.blade.php
+++ Milestone/resources/views/searchportfolios.blade.php
@@ -7,36 +7,39 @@
         	<div class="row justify-content-center">
             	<div class="col-md-8">
     				<div class="card">
-    					<div class="card-header">View Job</div>
-    						<div class="card-body">
+    					<div class="card-header">View Portfolio</div>
+    						<div class="card-body" align="center">
     							<form action="searchportfolios" method="post"> 
     								{{ csrf_field() }}
-    								<label for="pattern">Enter the name of the person you would like to search for</label>
+    								<label for="pattern">Enter the name of the user you would like to search for.</label><br/>
     								<input type="text" name="pattern" id="pattern"/>
-    								<input type="submit" value="Search"/>
+    								<input type="submit" value="Search" class="btn btn-primary"/>
     							</form>
-    								
+    								<div align="left">
     								@if(isset($portfolios))
-    									<h1>{{ $message }}</h1>
+    									<h1 class="card border border-secondary">{{ $message }}</h1>
     									@foreach ($portfolios as $id => $portfolio)
-    										<h3>Name: {{ $portfolio[0] }}</h3>
+    										<h3 class="card-header">Name: {{ $portfolio[0] }}</h3>
+    										<div class="card-body border border-secondary">
     										<h5>Education</h5>
     										@foreach ($portfolio[1]->getEducation() as $education)
-    											<a>Start Date: {{ $education['startdate'] }}</a>
-    											<a>End Date: {{ $education['enddate'] }}</a>
-    											<a>Institution: {{ $education['institution'] }}</a>
-    											<a>GPA: {{ $education['gpa'] }}</a>
+    											<a>Institution: {{ $education['institution'] }}</a><br/>
+    											<a>Start Date: {{ $education['startdate'] }}</a><br/>
+    											<a>End Date: {{ $education['enddate'] }}</a><br/>
+    											<a>GPA: {{ $education['gpa'] }}</a><hr>
     										@endforeach
     										<h5>Skills</h5>
     										@foreach ($portfolio[1]->getSkills() as $skill)
-    											<a>{{ $skill }}</a>
+    											<a>{{ $skill }}, </a>
     										@endforeach
-    										<h5>Job History</h5>
+    										<hr><h5>Job History</h5>
     										@foreach ($portfolio[1]->getHistory() as $history)
-    											<a>{{ $history }}</a>
+    											<a>{{ $history }}</a><br/>
     										@endforeach
+    										</div>
     									@endforeach
     								@endif
+    								</div>
     							</form>
     						</div>
     				</div>
